a6bd6b79516722411b685d24a4cb5b3baf5a7c9b
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2024-04-12 21:21:48 +0530

    Loading & App icon

--- my-app/src/components/Movie.js
+++ my-app/src/components/Movie.js
@@ -1,13 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "./Store/Context";
 
 const Movie = () => {
   const { movies, setMovies } = useContext(Context);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = async () => {
+    setIsLoading(true);
     const data = await fetch("https://swapi.dev/api/films/");
     const json = await data.json();
     const movieData = json?.results;
+    setIsLoading(false);
     setMovies(movieData);
   };
 
@@ -19,19 +22,25 @@ const Movie = () => {
       >
         Fetch Movie Data
       </button>
-      {movies.map((item) => (
-            <div key={item.episode_id}>
-              <div className="text-white w-[50%] m-auto mt-10 hover:scale-105 duration-500 border-2 border-white p-6">
-                <div className="flex justify-between">
-                  <h3 className="text-3xl font-bold">{item.title}</h3>
-                  <h1 className="text-3xl font-bold">
-                    Release: {item.release_date}
-                  </h1>
-                </div>
-                <h3 className="p-2 m-2 w-full ">{item.opening_crawl}</h3>
+      {isLoading ? (
+        <h1 className="text-6xl m-20 text-white">Searching for movies...</h1>
+      ) : (
+        movies.map((item) => (
+          <div key={item.episode_id}>
+            <div className="text-white w-[80%] md:w-[50%] m-auto mt-10 hover:scale-105 duration-500 border-2 border-white p-6">
+              <div className="flex justify-between">
+                <h3 className="text-3xl font-bold">{item.title}</h3>
+                <h1 className="text-3xl font-bold">
+                  Release: {item.release_date}
+                </h1>
               </div>
+              <p className="p-2 w-[100%] italic leading-relaxed">
+                {item.opening_crawl}
+              </p>
             </div>
-          ))}
+          </div>
+        ))
+      )}
     </div>
   );
 };
