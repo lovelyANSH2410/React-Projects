0afe24c6866d206df16b83a1d5621fe0e0f6df8a
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2024-04-13 13:11:01 +0530

    Added Movie form and Integrated firebase to store data

---
+++ my-app/src/components/AddMovieForm.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const AddMovieForm = (props) => {
+  const handleForm = async (e) => {
+    e.preventDefault();
+    const title = e.target.elements.title.value;
+    const openingText = e.target.elements.openingText.value;
+    const releaseDate = e.target.elements.releaseDate.value;
+
+    const newMovie = {
+      title: title,
+      opening_crawl: openingText,
+      release_date: releaseDate,
+    };
+
+    const response = await fetch(
+      "https://react-ecom-8f790-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(newMovie),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    props.toggleForm();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleForm}>
+        <input
+          className="bg-black text-white p-3 m-2 bg-opacity-50 border-2 border-white"
+          type="text"
+          name="title"
+          placeholder="Title"
+        />
+        <input
+          className="bg-black text-white p-3 m-2 bg-opacity-50 border-2 border-white"
+          type="text"
+          name="openingText"
+          placeholder="Opening Text"
+        />
+        <input
+          className="bg-black text-white p-3 m-2 bg-opacity-50 border-2 border-white"
+          type="text"
+          name="releaseDate"
+          placeholder="Release: YYYY-MM-DD"
+        />
+        <button
+          className="border-2 m-2 font-bold border-white p-2 rounded-md text-white hover:scale-105 duration-500 hover:opacity-80"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddMovieForm;
--- my-app/src/components/Movie.js
+++ my-app/src/components/Movie.js
@@ -1,26 +1,43 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { Context } from "./Store/Context";
+import AddMovieForm from "./AddMovieForm";
 
 const Movie = () => {
   const { movies, setMovies } = useContext(Context);
   const [isLoading, setIsLoading] = useState(false);
+  const [openForm, setOpenForm] = useState(false);
 
   const fetchData = useCallback(async () => {
     setIsLoading(true);
     try {
-      const data = await fetch("https://swapi.dev/api/films/");
+      const data = await fetch(
+        "https://react-ecom-8f790-default-rtdb.firebaseio.com/movies.json"
+      );
       const json = await data.json();
-      const movieData = json?.results;
+      const loadedMovies = [];
+
+      for (const key in json) {
+        loadedMovies.push({
+          episode_id: key,
+          title: json[key].title,
+          opening_crawl: json[key].opening_crawl,
+          release_date: json[key].release_date,
+        });
+      }
       setIsLoading(false);
-      setMovies(movieData);
+      setMovies(loadedMovies);
     } catch (error) {
       console.log("Error fetching data:", error);
     }
-  }, []);
+  }, [setMovies]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
+
+  const toggleForm = () => {
+    setOpenForm(!openForm);
+  };
 
   return (
     <div className="text-center mt-20 mb-40">
@@ -30,6 +47,13 @@ const Movie = () => {
       >
         Fetch Movie Data
       </button>
+      <button
+        className="border-2 m-2 font-bold border-white p-2 rounded-md text-white hover:scale-105 duration-500 hover:opacity-80"
+        onClick={toggleForm}
+      >
+        Add New
+      </button>
+      {openForm && <AddMovieForm toggleForm={toggleForm} />}
       {isLoading ? (
         <h1 className="text-6xl m-20 text-white">Searching for movies...</h1>
       ) : (
